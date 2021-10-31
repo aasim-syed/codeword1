08d020fa0b6588321a61b1a2db065ee1d764e86a
A.SYED AASIM <70385414+aasim-syed@users.noreply.github.com>
2021-10-31 19:26:27 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+
+import Navbar from './Components/Navbar';
+import Section from './Components/Section';
+import Logos from './Components/Logos';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Section/>
+    
+    </>
+  );
+}
+
+export default App;
